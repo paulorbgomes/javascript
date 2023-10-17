e439505f0f7072a4a17750d552746e55c1cfca82
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-17 17:05:54 -0300

    aula003 - tipos primitivos

---
+++ javascript-Andre_Iacono/aula003_tipos_primitivos.js
@@ -0,0 +1,20 @@
+// TIPOS PRIMITIVOS
+// String
+var nome = "Paulo R. B. Gomes";
+console.log(typeof(nome));
+
+// Number
+var numero = 10;
+console.log(typeof(numero));
+
+// Boolean
+var booleano = true;
+console.log(typeof(booleano));
+
+// Undefined
+var indefinida;
+console.log(indefinida);
+
+// Null
+var nula = null;
+console.log(nula);
