ba33d3785d3d5468abc0c9569d15000c3b4edf8e
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-17 10:41:07 -0300

    aulas 004 e 005

---
+++ javascript-devmedia/aula004_variaveis.js
@@ -0,0 +1,7 @@
+var variavel1 = "Paulo";
+var variavel2 = 10;
+var variavel3 = true;
+
+console.log(typeof(variavel1));
+console.log(typeof(variavel2));
+console.log(typeof(variavel3));
---
+++ javascript-devmedia/aula005_array.js
@@ -0,0 +1,12 @@
+var estados = ["Rio de Janeiro", "São Paulo", "Bahia"];
+
+console.log(estados);
+console.log(typeof(estados));
+
+console.log(estados[0]);
+console.log(estados[1]);
+console.log(estados[2]);
+
+estados[0] = "Ceará";
+console.log(estados);
+console.log(estados.length);
