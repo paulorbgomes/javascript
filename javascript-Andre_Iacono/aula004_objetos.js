0d3fe189f8062bc4e70cc096c38dfc4b19b894a3
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-17 17:30:36 -0300

    aula004 - objetos

---
+++ javascript-Andre_Iacono/aula004_objetos.js
@@ -0,0 +1,17 @@
+// OBJETOS
+var objeto = {
+    nome: "caneta",
+    preco: 30,
+    disponibilidade: true,
+    cor: "azul"
+}
+
+objeto.cor = "vermelho";
+
+console.log(typeof(objeto));
+console.log(objeto);
+
+console.log(objeto.nome);
+console.log(objeto.preco);
+console.log(objeto.disponibilidade);
+console.log(objeto.cor);
