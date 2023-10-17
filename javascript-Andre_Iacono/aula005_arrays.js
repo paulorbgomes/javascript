5f553c002cff44d676740d3d51c2ad1252dabfc7
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-17 17:42:24 -0300

    aula005 - arrays

---
+++ javascript-Andre_Iacono/aula005_arrays.js
@@ -0,0 +1,13 @@
+// Arrays
+var amigos = ["Flávia", "Paulo", "Matheus"];
+console.log(typeof(amigos));
+console.log(amigos);
+
+// Acesso ao elemento do array
+console.log(amigos[0]);
+console.log(amigos[1]);
+console.log(amigos[2]);
+
+// Modificar o elemento do array
+amigos[1] = "Gomes";
+console.log(amigos);
