7f7e351768edae38c424164649b96aee3a1bb831
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-17 10:13:04 -0300

    aula003 - variaveis.js

---
+++ javascript-devmedia/aula003_variaveis.js
@@ -0,0 +1,10 @@
+// Variaveis
+var programador = "Paulo";
+var pontuacao = 10;
+var idade = 33;
+
+// Saida
+console.log(programador);
+console.log(pontuacao);
+console.log(idade);
+console.log("Nome: " + programador + ", Idade: " + idade + ", Pontuacao: " + pontuacao);
