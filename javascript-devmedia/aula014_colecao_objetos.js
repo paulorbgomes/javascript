44d0f2b7b2128e150fdacc40aa9c872a7f11ab03
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-18 00:11:38 -0300

    aula014 - colecao de objetos

---
+++ javascript-devmedia/aula014_colecao_objetos.js
@@ -0,0 +1,9 @@
+var cadastro = [
+    {nome:"Paulo", idade:33, cidade:"Fortaleza"},
+    {nome:"Ricardo", idade:33, cidade:"Taua"}
+];
+
+console.log(cadastro);
+console.log(cadastro[0].nome);
+console.log(cadastro[0].idade);
+console.log(cadastro[0].cidade);
