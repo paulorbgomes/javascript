b519ef94a35b9a6b499759368f9be8d232f48ac4
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-18 09:32:04 -0300

    estruturas condicionais em javascript

---
+++ javascript-devmedia/aula015_if_else.js
@@ -0,0 +1,9 @@
+var valorProduto = 100;
+var saldoConta = 500;
+
+if(saldoConta >= valorProduto){
+    console.log("Compra Aprovada!");
+}
+else{
+    console.log("Compra Rejeitada!");
+}
---
+++ javascript-devmedia/aula016_if_elseif_else.js
@@ -0,0 +1,12 @@
+var possibilidades = ["verde", "amarelo", "vermelho"];
+var semaforo = possibilidades[0];
+
+if(semaforo == "verde"){
+    console.log("Siga!");
+}
+else if(semaforo == "amarelo"){
+    console.log("Atencao!");
+}
+else{
+    console.log("Pare!");
+}
