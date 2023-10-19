e5ff84b02fe681b848206cd0301c6618b9a0fb8c
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-19 11:00:09 -0300

    projeto001 - juros em uma divida

---
+++ javascript-devmedia/projeto001-juros_divida/projeto001.js
@@ -0,0 +1,25 @@
+// Sistema que calcula os juros de uma dívida com base no número de dias passados
+
+import input from 'readline-sync';
+
+// Entrada
+let valor = Number(input.question("Informe o valor devido: R$ "));
+let dias = Number(input.question("Informe quantos dias se passaram desde o vencimento do boleto: "));
+
+// Processamento
+let valorTotal = null;
+let juros = null;
+if(dias > 15){
+    juros = 10;
+    valorTotal = valor + (valor * juros / 100);
+}
+else{
+    juros = 5;
+    valorTotal = valor + (valor * juros / 100);
+}
+
+// Saida
+console.log("Valor original da divida: R$ " + valor);
+console.log("Dias de atraso: " + dias);
+console.log("Taxa de juros: " + juros + "%");
+console.log("Valor total com juros: R$ " + valorTotal);
