604dabc20c688e09240be7bab5e535c90ec1fcca
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-19 14:01:39 -0300

    aula019 - arrays

---
+++ javascript-devmedia/aula019_arrays.js
@@ -0,0 +1,8 @@
+// Declaracao e atribuicao de um array (variavel composta e heterogenea)
+let programadores = ["Paulo", "Alexandre", "Lucas", "Saulo"];
+
+console.log(typeof(programadores));
+console.log(programadores);
+programadores[0] = "Ricardo";
+console.log(programadores);
+console.log(programadores.length);
