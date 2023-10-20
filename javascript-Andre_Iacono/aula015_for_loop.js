a34e534e47e24918fc97397a0484cbfacd96b4e4
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-20 12:06:10 -0300

    for loop

---
+++ javascript-Andre_Iacono/aula015_for_loop.js
@@ -0,0 +1,13 @@
+// LOOP FOR
+/*
+console.log("Numeros pares entre 0 e 100:");
+for(var i = 0; i <= 100; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+*/
+
+for(var num = 10; num <= 20; num++){
+    console.log(num);
+}
