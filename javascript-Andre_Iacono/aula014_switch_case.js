50a16e91cf38df5d010c3fb95353f6a41da1bc07
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-20 11:59:16 -0300

    aula switch case

---
+++ javascript-Andre_Iacono/aula014_switch_case.js
@@ -0,0 +1,19 @@
+// SWITCH E CASE
+let aeroporto = null;
+//aeroporto = "MCO";
+//aeroporto = "JFK";
+aeroporto = "SEA";
+
+switch(aeroporto){
+    case "MCO":
+        console.log("Orlando ...");
+        break;
+    case "JFK":
+        console.log("J. F. Kennedy ...");
+        break;
+    case "SEA":
+        console.log("Seatle ...");
+        break;
+    default:
+        console.log("Opcao Invalida!");
+}
