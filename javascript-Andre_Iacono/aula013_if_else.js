b5563f7e23ad3757a5f67bcb0efb97231b4c776d
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-20 11:52:42 -0300

    aula if_elseif_else

---
+++ javascript-Andre_Iacono/aula013_if_else.js
@@ -0,0 +1,12 @@
+// IF / ELSE
+let piloto = 80;
+
+if(piloto > 110){
+    console.log("Muito Rápido!");
+}
+else if(piloto >= 40 && piloto <= 110){
+    console.log("Velocidade OK!");
+}
+else{
+    console.log("Muito Lento!");
+}
