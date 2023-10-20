b4b501e322f3b2c326a51e8dfec640b10b855fb9
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-20 18:05:27 -0300

    aula loop while

---
+++ javascript-Andre_Iacono/aula016_while_loop.js
@@ -0,0 +1,13 @@
+// LOOP WHILE
+
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+console.log("\n");
+
+let contador = 1;
+while(contador <= 10){
+    console.log(contador);
+    contador++;
+}
