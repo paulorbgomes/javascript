a200cf0c7e5b4398f7b78524af8b98747d8a6703
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-20 11:15:02 -0300

    operadores de comparacao

---
+++ javascript-Andre_Iacono/aula010_operadores_comparacao.js
@@ -0,0 +1,16 @@
+// OPERADORES DE COMPARACAO
+let num1 = 5;
+let num2 = 3;
+
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
+
+console.log(num1 == "5");
+console.log(num1 != "5");
+
+console.log(num1 === "5");
+console.log(num1 !== "5");
