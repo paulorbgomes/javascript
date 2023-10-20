b7ad508587a1e5ac9cc1d4691d95cfd9d118e979
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-20 11:41:21 -0300

    operadores logicos

---
+++ javascript-Andre_Iacono/aula012_operadores_logicos.js
@@ -0,0 +1,21 @@
+// OPERADORES LOGICOS
+// TABELA VERDADE (AND)
+console.log("Tabela Verdade (AND)");
+console.log(false && false);
+console.log(false && true);
+console.log(true && false);
+console.log(true && true);
+console.log("\n");
+
+// TABELA VERDADE (OR)
+console.log("Tabela Verdade (OR)");
+console.log(false || false);
+console.log(false || true);
+console.log(true || false);
+console.log(true || true);
+console.log("\n");
+
+// TABELA VERDADE (NOT)
+console.log("Tabela Verdade (NOT)");
+console.log(!true);
+console.log(!false);
