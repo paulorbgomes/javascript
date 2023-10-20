47ec47ff6371bb8ccaf48bb5c97611881219e078
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-20 10:58:43 -0300

    operadores aritmeticos

---
+++ javascript-Andre_Iacono/aula008_operadores_aritmeticos.js
@@ -0,0 +1,16 @@
+// OPERADORES ARITMETICOS
+let num1 = 3;
+let num2 = 2;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+//console.log(++num1);
+//console.log(--num1);
+
+//console.log(num1++);
+//console.log(num1);
