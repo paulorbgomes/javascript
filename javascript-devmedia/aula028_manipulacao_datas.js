f7988b4b2f24980e32676ad7f5b434f59ebaf04c
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-22 10:38:04 -0300

    aula028 - datas

---
+++ javascript-devmedia/aula028_manipulacao_datas.js
@@ -0,0 +1,15 @@
+const data = new Date();
+console.log(data);
+console.log(typeof(data));
+console.log(data.toString());
+console.log(data.toUTCString());
+console.log(data.valueOf());
+console.log(data.getFullYear());
+console.log(data.getMonth());
+console.log(data.getDate());
+console.log(data.getHours());
+console.log(data.getMinutes());
+console.log(data.getSeconds());
+console.log(data.getMilliseconds());
+console.log(data.getDay());
+console.log(data.getTime());
