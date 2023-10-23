7a77506cda6fbd94e81afa9cee7dab9ef56d1b57
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-22 22:38:07 -0300

    atualizacao arrow function

--- javascript-devmedia/aula029_funcoes.js
+++ javascript-devmedia/aula029_funcoes.js
@@ -12,3 +12,18 @@ const valor = retorna();
 console.log(valor);
 
 // Funcao com parametro ...
+function olaP(nome){
+    console.log(`Ola, ${nome}!`);
+}
+olaP("Paulo Gomes");
+
+// Arrow Function
+/*
+function media(n1, n2){
+    return (n1 + n2)/2;
+}
+let valorMedia= media(10, 9);
+console.log(valorMedia);
+*/
+const media = (n1, n2) => (n1 + n2)/2;
+console.log(media(8, 9));
