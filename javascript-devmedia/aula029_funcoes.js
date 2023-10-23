d3cd00bfa88846f3883a035f68f92d6a7f60e92e
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-22 22:03:14 -0300

    aula029 - funcoes

---
+++ javascript-devmedia/aula029_funcoes.js
@@ -0,0 +1,14 @@
+// Funcao sem parametro ...
+function ola(){
+    console.log("Ola! Seja bem-vindo!");
+};
+
+function retorna(){
+    return 100;
+};
+
+ola();
+const valor = retorna();
+console.log(valor);
+
+// Funcao com parametro ...
