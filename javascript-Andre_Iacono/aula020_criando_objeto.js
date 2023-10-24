95067a56e15778ac1fdab849b8d07af3fea12edb
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-24 08:39:08 -0300

    aula020 - criando objetos e funcoes

---
+++ javascript-Andre_Iacono/aula020_criando_objeto.js
@@ -0,0 +1,17 @@
+let livro = {
+    titulo: "Habitos Atomicos",
+    autor: "James Clear",
+    paginas: 306,
+    capitulos: {
+        cap1: "Fundamentos", 
+        cap2: "Leis"
+    },
+
+    imprimir: function(){
+        console.log("Imprimindo ...");
+    }
+};
+
+livro.imprimir();
+
+
