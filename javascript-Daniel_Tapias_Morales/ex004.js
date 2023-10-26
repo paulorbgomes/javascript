2076c2fa9799065e30b27d67ef194be7973844da
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-26 14:38:10 -0300

    ex004

--- javascript-Daniel_Tapias_Morales/ex004.js
+++ javascript-Daniel_Tapias_Morales/ex004.js
@@ -1,1 +1,23 @@
-let num = prompt("Por favor, digite um numero de 1 a 6: ");
+let num = Math.round(Number(prompt("Por favor, digite um numero de 1 a 6: ")));
+switch(num){
+    case 1:
+        alert("Voce digitou o numero 1!");
+        break;
+    case 2:
+        alert("Voce digitou o numero 2!");
+        break;
+    case 3:
+        alert("Voce digitou o numero 3!");
+        break;
+    case 4:
+        alert("Voce digitou o numero 4!");
+        break;
+    case 5:
+        alert("Voce digitou o numero 5!");
+        break;
+    case 6: 
+        alert("Voce digitou o numero 6!");
+        break;
+    default:
+        alert("Valor invalido para esta aplicacao! Atencao!");
+}
