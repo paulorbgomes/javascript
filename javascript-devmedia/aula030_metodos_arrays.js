0f5fa6853fbcd9528b748429e208df9fe39f1f64
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-10-31 15:18:27 -0300

    metodos em arrays

---
+++ javascript-devmedia/aula030_metodos_arrays.js
@@ -0,0 +1,18 @@
+const listaNomes = ["Paulo", "Gomes", "Ricardo", "Pedro"];
+const mostraNomes = (nome) => console.log(nome);
+// forEach() ...
+listaNomes.forEach(mostraNomes);
+
+// filter() ...
+const verificaNome = (nome) => nome == "Paulo" ? true : false;
+const novoArray = listaNomes.filter(verificaNome);
+console.log(novoArray);
+
+// map() ...
+const agenda = [
+    {nome: "Paulo", telefone: "tel01"},
+    {nome: "Gomes", telefone: "tel02"},
+    {nome: "Ricardo", telefone: "tel03"},
+];
+const novaAgenda = agenda.map((contato) => contato.telefone);
+console.log(novaAgenda);
