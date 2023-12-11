6f81e3cde189a329c4c9e38528defcaba4bbb22d
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-11 09:39:15 -0300

    Aula: Variáveis e Constantes

---
+++ javascript-Carlos_Junior_Uchoa_Oliveira/secao01 - Introducao ao JS/aula002 - Variaveis e Constantes/variaveis_constantes.js
@@ -0,0 +1,18 @@
+// var ...
+var nome = "Paulo Ricardo Barboza Gomes";
+console.log(nome);
+nome = "Paulo R. B. Gomes";
+console.log(nome);
+
+// let ...
+{
+  let idade = 34;
+  console.log(idade);
+  console.log(nome);
+}
+console.log(nome);
+
+// const ...
+const local = "IFCE";
+//local = "IFCE-Tauá";
+console.log(local);
