86e11133d8b130e421f7671b29959a83ae89b796
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-11 11:09:26 -0300

    Aula: Tipos primitivos do JS

---
+++ javascript-Carlos_Junior_Uchoa_Oliveira/secao01 - Introducao ao JS/aula003 - Tipos primitivos/tipos_primitivos.js
@@ -0,0 +1,28 @@
+// Tipo String
+let string = "Isso é uma string";
+console.log(string);
+console.log(typeof string);
+
+// Tipo Number
+let numero1 = 10;
+console.log(numero1);
+console.log(typeof numero1);
+
+let numero2 = 10.56;
+console.log(numero2);
+console.log(typeof numero2);
+
+// Tipo Boolean
+let booleano = true;
+//booleano = false;
+console.log(booleano);
+console.log(typeof booleano);
+
+// Tipo Undefined;
+let indefinido;
+console.log(typeof indefinido);
+
+// Tipo Null
+let nulo = null;
+console.log(nulo);
+console.log(typeof nulo);
