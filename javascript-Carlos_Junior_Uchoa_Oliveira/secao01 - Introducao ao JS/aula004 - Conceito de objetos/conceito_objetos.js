8b0e709ce7a15d39a04587d7086979edb5767512
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-11 19:53:35 -0300

    Assunto: Conceito de objeto em JS

---
+++ javascript-Carlos_Junior_Uchoa_Oliveira/secao01 - Introducao ao JS/aula004 - Conceito de objetos/conceito_objetos.js
@@ -0,0 +1,17 @@
+// Objeto em JS
+let carro = {
+  modelo: "Mobi",
+  marca: "Fiat",
+  ano: 2017,
+  cor: "Vermelho",
+};
+
+console.log(carro);
+console.log(carro.modelo);
+console.log(typeof carro.modelo);
+console.log(carro.marca);
+console.log(typeof carro.marca);
+console.log(carro.ano);
+console.log(typeof carro.ano);
+console.log(carro.cor);
+console.log(typeof carro.cor);
