822bda360a587b1042903e5c722503f69282775b
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-11 21:27:24 -0300

    Assunto: Strings em JA

---
+++ javascript-Carlos_Junior_Uchoa_Oliveira/secao02 - Strings em JS/aula004 - Template strings/index.js
@@ -0,0 +1,10 @@
+console.log("Template Strings em JS");
+const nome = "Paulo R. B. Gomes";
+const profissao = "Professor";
+console.log(`Nome: ${nome}; Profissão: ${profissao}`);
+
+console.log(`Caixa d'água do Beltrano que falou: "..."`);
+
+console.log(`Template Strings
+             permitem quebra de linha
+             automática!`);
---
+++ javascript-Carlos_Junior_Uchoa_Oliveira/secao02 - Strings em JS/aula005 - Interpolacao/index.js
@@ -0,0 +1,3 @@
+const nome = "Paulo R. B. Gomes";
+const idade = 2023 - 1989;
+console.log(`Nome: ${nome}, Idade: ${idade} anos`);
