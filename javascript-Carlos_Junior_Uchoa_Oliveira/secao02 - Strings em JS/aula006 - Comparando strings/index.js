e11aea19cd8b79a3be7ccb20d1c24d8bc0d224ab
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-11 21:44:28 -0300

    Assunto: Strings em JS

---
+++ javascript-Carlos_Junior_Uchoa_Oliveira/secao02 - Strings em JS/aula006 - Comparando strings/index.js
@@ -0,0 +1,10 @@
+const string1 = "String";
+const string2 = "STRING";
+const string3 = "String";
+
+console.log(string1 == string2);
+console.log(string1 == string3);
+
+console.log("a".localeCompare("b", "pt-BR"));
+console.log("b".localeCompare("a", "pt-BR"));
+console.log("a".localeCompare("a", "pt-BR"));
