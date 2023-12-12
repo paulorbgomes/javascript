73e30b99a62d9a52df4899ad3beb14eae673095b
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-11 21:08:28 -0300

    Assunto: Strings em JS

---
+++ javascript-Carlos_Junior_Uchoa_Oliveira/secao02 - Strings em JS/aula003 - Escape/index.js
@@ -0,0 +1,14 @@
+console.log(" ' ");
+console.log("Caixa D'água");
+
+console.log(" ' ");
+console.log("Caixa D'água");
+
+console.log(' " ');
+console.log('O personagem falou: "citação ..."');
+
+console.log("Quebra \nDe \nLinha");
+
+console.log("Paulo\tGomes");
+
+console.log(" \\ ");
