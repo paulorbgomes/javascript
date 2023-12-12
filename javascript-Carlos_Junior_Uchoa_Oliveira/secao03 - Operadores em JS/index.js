a87d806b3013a6e3c7f4020244690aee3f5510b4
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-12 11:38:05 -0300

    Assunto: Operadores em JS

---
+++ javascript-Carlos_Junior_Uchoa_Oliveira/secao03 - Operadores em JS/index.js
@@ -0,0 +1,69 @@
+/*
+// Operadores Aritméticos
+let num1 = Number(prompt("Digite o primeiro número: "));
+let num2 = Number(prompt("Digite o segundo número: "));
+alert(`A soma entre ${num1} e ${num2} vale: ${num1 + num2}
+A subtração entre ${num1} e ${num2} vale: ${num1 - num2}
+A multiplicação entre ${num1} e ${num2} vale: ${num1 * num2}
+A divisão entre ${num1} e ${num2} vale: ${num1 / num2}
+O valor de ${num1} elevado a ${num2} é: ${num1 ** num2}
+O resto da divisão entre ${num1} e ${num2} vale: ${num1 % num2}`);
+*/
+
+/*
+// Operadores de Incremento e Decremento
+let valor = 10;
+console.log(valor++);
+console.log(++valor);
+console.log(valor--);
+console.log(--valor);
+*/
+
+/*
+// Operador Resto da Divisão
+let num1 = 10;
+let num2 = 3;
+console.log(num1 % num2);
+*/
+
+/*
+// Operador de Atribuição
+let num1 = 20;
+let num2 = 10;
+let num3 = num1;
+
+num1 += 1;
+num1 -= 1;
+num1 *= 2;
+num1 /= 10;
+console.log(num1);
+*/
+
+/*
+// Operadores de Comparação
+// == , === , != , !== , > , >= , < , <=
+let num1 = 10;
+let num2 = "10";
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 != num2);
+console.log(num1 !== num2);
+*/
+
+// Operadores Lógicos
+// && , || , !
+console.log("--- AND ---");
+console.log(false && false); //false
+console.log(false && true); //false
+console.log(true && false); //false
+console.log(true && true); //true
+
+console.log("--- OR ---");
+console.log(false || false); //false
+console.log(false || true); //true
+console.log(true || false); //true
+console.log(true || true); //true
+
+console.log("--- NOT ---");
+console.log(!false); //true
+console.log(!true);  //flase
