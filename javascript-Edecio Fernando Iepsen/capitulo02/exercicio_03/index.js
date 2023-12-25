99747a8a0bcba420462210e5ffd8e6477c3b3cd7
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2023-12-25 20:58:56 -0300

    Exercícios JavaScript

--- javascript-Edecio Fernando Iepsen/capitulo02/exercicio_03/index.js
+++ javascript-Edecio Fernando Iepsen/capitulo02/exercicio_03/index.js
@@ -4,13 +4,17 @@ function verPromocao() {
   let preco = Number(document.getElementById("preco").value);
 
   //Processamento
-  let preco03 = Number(preco / 2).toFixed(2);
+  let preco03 = Number((preco / 2).toFixed(2));
   let precoPromocao = 2 * preco + preco03;
 
   //Saida
   let saida01 = document.getElementById("saida01");
   let saida02 = document.getElementById("saida02");
 
-  saida01.innerHTML = `<b>${produto} - Promoção: Leve 3 por R$: ${precoPromocao}</b>`;
-  saida02.innerHTML = `<b>O 3º produto custa apenas R$: ${preco03}</b>`;
+  saida01.innerHTML = `<b>${produto} - Promoção: Leve 3 por R$: ${precoPromocao.toFixed(
+    2
+  )}</b>`;
+  saida02.innerHTML = `<b>O 3º produto custa apenas R$: ${preco03.toFixed(
+    2
+  )}</b>`;
 }
